430be48bfe9c291d62b72ca0fd4ec01e316050d4
Marcelo A Coldibelli <marcelo.coldibelli@gmail.com>
2024-08-05 17:00:06 -0300

    feat: add transfer table to dbContext

--- api/Data/ApplicationDbContext.cs
+++ api/Data/ApplicationDbContext.cs
@@ -6,4 +6,22 @@ namespace api.Data;
 public class ApplicationDbContext(DbContextOptions dbContextOptions) : DbContext(dbContextOptions)
 {
     public DbSet<User> User { get; set; }
+    public DbSet<Transfer> Transfer { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Transfer>()
+            .HasOne(t => t.Payer)
+            .WithMany()
+            .HasForeignKey(t => t.PayerId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<Transfer>()
+            .HasOne(t => t.Payee)
+            .WithMany()
+            .HasForeignKey(t => t.PayeeId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
 }
