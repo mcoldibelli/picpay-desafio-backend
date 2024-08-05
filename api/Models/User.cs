62e51c10ccf03273a8562cac05c3ae194ba39a0b
Marcelo A Coldibelli <marcelo.coldibelli@gmail.com>
2024-08-04 23:34:57 -0300

    refactor: corrects user model

--- api/Models/User.cs
+++ api/Models/User.cs
@@ -4,31 +4,36 @@ using api.Models.Enums;
 
 namespace api.Models;
 
+[Table("Users")]
 public class User
 {
     [Key]
-    public int Id { get; private set; }
+    public int Id { get; set; }
 
     [Required(ErrorMessage = "First name is required")]
     [StringLength(50, ErrorMessage = "First name cannot exceed 50 characters")]
-    public string FirstName { get; private set; }
+    public string FirstName { get; set; } = string.Empty;
 
     [Required(ErrorMessage = "Last name is required")]
     [StringLength(50, ErrorMessage = "Last name cannot exceed 50 characters")]
-    public string LastName { get; private set; }
+    public string LastName { get; set; } = string.Empty;
 
-    [Required(ErrorMessage = "Document type is required")]
-    public DocumentType Document { get; private set; }
+    [Required(ErrorMessage = "Document is required")]
+    [StringLength(14, MinimumLength = 11, ErrorMessage = "Document must be a CNPJ or a CPF format")]
+    public string Document { get; private set; } = string.Empty;
 
     [Required(ErrorMessage = "Email is required")]
     [StringLength(100, ErrorMessage = "Email cannot exceed 100 characters")]
     [EmailAddress(ErrorMessage = "Invalid email address")]
-    public string Email { get; private set; }
+    public string Email { get; set; } = string.Empty;
 
     [Required(ErrorMessage = "Password is required")]
     [StringLength(100, MinimumLength = 12, ErrorMessage = "Password must be between 12 and 100 characters")]
-    public string Password { get; private set; }
+    public string Password { get; set; } = string.Empty;
 
     [Column(TypeName = "decimal(18,2)")]
     public decimal Balance { get; private set; }
+
+    [Required(ErrorMessage = "User type is required")]
+    public UserType UserType;
 }
